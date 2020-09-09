41cf4aff12d9b2d49e1edfd1b8e6bd21ca623e16
Anna Vu <ngavu2712@gmail.com>
2020-09-08 20:27:11 -0700

    create some basic components and install yarn package manager

---
+++ client/src/components/App/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Homepage from '../Homepage'
+import Login from '../Login'
+import Register from '../Register'
+import Dashboard from '../Dashboard'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline } from '@material-ui/core'
+import 
