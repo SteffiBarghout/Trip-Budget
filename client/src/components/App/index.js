a82db12c779f72a1c56ab4729364fccff0d2beb7
Anna Vu <ngavu2712@gmail.com>
2020-09-08 20:36:36 -0700

    set up the app.js with router and path to the authentication, homepage and dashboard

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -5,4 +5,21 @@ import Register from '../Register'
 import Dashboard from '../Dashboard'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { CssBaseline } from '@material-ui/core'
-import 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+export default function App() {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />  {/* resets the style accross brower */}
+            <Router> {/* from react-router-dom, allow you to add path */}
+                <Switch>
+                    <Route exact path='/' component={Homepage}/>
+                    <Route exact path='/register' component={Register}/>
+                    <Route exact path='/login' component={Login}/>
+                    <Route exact path='/dashboard' component={Dashboard}/>
+                </Switch>
+            </Router>
+           
+        </MuiThemeProvider>
+    )
+}
